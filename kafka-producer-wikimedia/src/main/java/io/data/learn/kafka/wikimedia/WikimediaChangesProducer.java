1edaae7762a1fca5d863f2bdb4c00b5182de241b
thupla <thuleanhphan@gmail.com>
2025-05-28 17:19:45 +0700

    [producer] feat: high throughput config

--- kafka-producer-wikimedia/src/main/java/io/data/learn/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/data/learn/kafka/wikimedia/WikimediaChangesProducer.java
@@ -28,6 +28,10 @@ public class WikimediaChangesProducer {
 //        properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG);
 //        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
+        // high throughput config
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); //32KB
 
         // create producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
